82144968cde485db1fb46774e1aaf4381fff79c7
as <athishsreeram007@gmail.com>
2019-01-20 02:53:50 +0530

    SBA 2.1.2 - Consul

---
+++ src/test/java/com/springcloud/example/employee/EmployeeApplicationTests.java
@@ -0,0 +1,17 @@
+package com.springcloud.example.employee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
