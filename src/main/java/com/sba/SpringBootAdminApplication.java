7184e91797287f6242e08d8d9ae2b7ad3eb7b59e
as <athishsreeram007@gmail.com>
2019-01-20 07:43:57 +0530

    # sba
    Employee SBA App
    
    SB
    2.1.2.RELEASE
    
    Consul
    2.0.1.RELEASE
    
    SBA
    2.1.2
    
    Java
    1.8

--- src/main/java/com/sba/SpringBootAdminApplication.java
+++ src/main/java/com/sba/SpringBootAdminApplication.java
@@ -1,8 +1,9 @@
 package com.sba;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
-import org.springframework.boot.SpringApplication;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +11,10 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableAdminServer
+@SpringBootApplication
 public class SpringBootAdminApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootAdminApplication.class, args);
+        new SpringApplicationBuilder(SpringBootAdminApplication.class)
+                .run(args);
     }
 }
